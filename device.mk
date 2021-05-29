c5d84d2302492969be13bc446c617222148a7313
Zinadin Zidan <zidan44@pixelexperience.org>
2021-05-29 18:21:28 +0530

    RMX2185: Import MTk IMS Blobs from realmeUI 2.0
    * RMX2185_11_C.05
    
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -78,6 +78,20 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
+# ImsInit hack
+PRODUCT_PACKAGES += \
+    ImsInit
+
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
 # Init
 PRODUCT_PACKAGES += \
     init.mt6765.rc \
