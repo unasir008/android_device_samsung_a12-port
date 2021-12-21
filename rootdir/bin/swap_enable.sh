f5e8ef5accf220d38f0d35bc1ed5f42303179c15
Aryan Sinha <sinha.aryan03@gmail.com>
2021-12-21 15:31:13 +0000

    RMX2185: rootdir: swap_enable: Update from realmeUI 2.0 * from RMX2185_11_C.08
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- rootdir/bin/swap_enable.sh
+++ rootdir/bin/swap_enable.sh
@@ -1,42 +1,52 @@
 #!/system/bin/sh
 #
-#ifdef VENDOR_EDIT
-#huacai.zhou@PSW.BSP.kernel.drv, 2018/03/09, adjust zram size according to total ram size
-MemTotalStr=`cat /proc/meminfo | grep MemTotal`
-MemTotal=${MemTotalStr:16:8}
+#ifdef OPLUS_FEATURE_ZRAM_OPT
+    MemTotalStr=`cat /proc/meminfo | grep MemTotal`
+    MemTotal=${MemTotalStr:16:8}
 
-if [ $MemTotal -le 2097152 ]; then
-  #config 1.25GB zram size with memory less than 2 GB
-  echo zstd > /sys/block/zram0/comp_algorithm
-  echo 1342177280 > /sys/block/zram0/disksize
-  echo 180 > /proc/sys/vm/swappiness
-  echo 0 > /proc/sys/vm/direct_swappiness
-  echo 10 > /proc/sys/vm/watermark_scale_factor
-elif [ $MemTotal -le 3145728 ]; then
-  #config 1.6GB zram size with memory less than 3 GB
-  echo zstd > /sys/block/zram0/comp_algorithm
-  echo 1717986918 > /sys/block/zram0/disksize
-  echo 180 > /proc/sys/vm/swappiness
-  echo 0 > /proc/sys/vm/direct_swappiness
-elif [ $MemTotal -le 4194304 ]; then
-  #config 2GB zram size with memory less than 4 GB
-  echo lz4 > /sys/block/zram0/comp_algorithm
-  echo 2147483648 > /sys/block/zram0/disksize
-  echo 160 > /proc/sys/vm/swappiness
-  echo 60 > /proc/sys/vm/direct_swappiness
-elif [ $MemTotal -le 6291456 ]; then
-  #config 1GB zram size with memory 6 GB
-  echo lz4 > /sys/block/zram0/comp_algorithm
-  echo 2415919104 > /sys/block/zram0/disksize
-  echo 160 > /proc/sys/vm/swappiness
-  echo 60 > /proc/sys/vm/direct_swappiness
-else
-  #config 1GB zram size with memory greater than 6 GB
-  echo lz4 > /sys/block/zram0/comp_algorithm
-  echo 2415919104 > /sys/block/zram0/disksize
-  echo 160 > /proc/sys/vm/swappiness
-  echo 60 > /proc/sys/vm/direct_swappiness
-fi
-mkswap /dev/block/zram0
-swapon /dev/block/zram0
-#endif /* VENDOR_EDIT */
+    echo lz4 > /sys/block/zram0/comp_algorithm
+    echo 160 > /proc/sys/vm/swappiness
+    echo 60 > /proc/sys/vm/direct_swappiness
+    echo 0 > /proc/sys/vm/page-cluster
+    if [ -f /sys/block/zram0/disksize ]; then
+        if [ -f /sys/block/zram0/use_dedup ]; then
+            echo 1 > /sys/block/zram0/use_dedup
+        fi
+
+        if [ $MemTotal -le 524288 ]; then
+            #config 384MB zramsize with ramsize 512MB
+            echo 402653184 > /sys/block/zram0/disksize
+        elif [ $MemTotal -le 1048576 ]; then
+            #config 768MB zramsize with ramsize 1GB
+            echo 805306368 > /sys/block/zram0/disksize
+        elif [ $MemTotal -le 2097152 ]; then
+            #config 1.25GB zram size with memory less than 2 GB
+            echo zstd > /sys/block/zram0/comp_algorithm
+            echo 1342177280 > /sys/block/zram0/disksize
+            echo 180 > /proc/sys/vm/swappiness
+            echo 0 > /proc/sys/vm/direct_swappiness
+            echo 15 > /proc/sys/vm/watermark_scale_factor
+            # set min_free_kbytes = 10MB
+            echo 10240 > /proc/sys/vm/min_free_kbytes
+            #endif
+        elif [ $MemTotal -le 3145728 ]; then
+            #config 1.6GB zram size with memory less than 3 GB
+            echo zstd > /sys/block/zram0/comp_algorithm
+            echo 1717986918 > /sys/block/zram0/disksize
+            echo 180 > /proc/sys/vm/swappiness
+            echo 0 > /proc/sys/vm/direct_swappiness
+        elif [ $MemTotal -le 4194304 ]; then
+            #config 2GB+512MB zramsize with ramsize 4GB
+            echo 2147483648 > /sys/block/zram0/disksize
+        elif [ $MemTotal -le 6291456 ]; then
+            #config 3GB zramsize with ramsize 6GB
+            echo 2415919104 > /sys/block/zram0/disksize
+        else
+            #config 4GB zramsize with ramsize >=8GB
+            echo 4294967296 > /sys/block/zram0/disksize
+        fi
+        /vendor/bin/mkswap /dev/block/zram0
+        /vendor/bin/swapon /dev/block/zram0
+    fi
+
+#endif /* OPLUS_FEATURE_ZRAM_OPT */
