Irawans-Android-Lab/device_realme_RMX2185

RMX2185: Disable Soundtrigger via vendor overlay
* for Q devices not using hotword enrollment apks it is necessary to do this if using pixel props
* this solution is cleaner than overlaying entire manifest.xml

Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>

device.mk
@@ -152,6 +152,10 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
+# Vendor overlay
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/vendor-overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION))
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \

unasir008/android_device_samsung_a12-port

Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

device.mk
@@ -165,6 +165,10 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 PRODUCT_PACKAGES += \
     libshim_showlogo
 
+# Vendor overlay
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/vendor-overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_EXTRA_VNDK_VERSIONS))
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \