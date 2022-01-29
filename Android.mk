3c9def0a4240e0e65a97bfa177340b4255c8714f
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-01-29 11:50:26 +0000

    RMX2185: Make Android.mk search recursive
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- Android.mk
+++ Android.mk
@@ -19,5 +19,6 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter RMX2185,$(TARGET_DEVICE)),)
 $(call add-radio-file,dynamic-remove-oppo)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
