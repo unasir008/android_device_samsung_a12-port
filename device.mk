6f1146dc51cb1158d42aa5162258fc639bc3f38b
techyminati <sinha.aryan03@gmail.com>
2022-04-04 20:03:18 +0530

    RMX2185: Move Permissions to configs
    Change-Id: I87335b32aa3ede0f90fa7831b8e57313e55dbf2a
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.controls.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.controls.xml \
-    $(DEVICE_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
 # Properties
 -include $(DEVICE_PATH)/system_prop.mk
