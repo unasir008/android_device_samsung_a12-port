a9bcf88c1600bbab7ef2b307b8b8d1ed0a3581de
Aryan Sinha <sinha.aryan03@gmail.com>
2022-04-16 15:56:28 +0530

    RMX2185: vendorsetup: Sync Linaro GCC as well

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,5 +4,7 @@ git clone https://github.com/techyminati/proprietary_vendor_realmeme_RMX2185 -b 
 git clone https://gitlab.com/techyminati/karashi -b android-11.0 kernel/realme/mt6765G
 git clone https://github.com/techyminati/android_packages_apps_RealmeParts -b lineage-19.1 packages/apps/RealmeParts
 git clone https://github.com/techyminati/android_packages_apps_RealmeDirac  -b lineage-19.1 packages/apps/RealmeDirac
+git clone https://github.com/techyminati/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-gnu-7.5 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-7.5 --depth=1
+git clone https://github.com/techyminati/android_prebuilts_gcc_linux-x86_arm_arm-linux-gnueabihf-7.5 prebuilts/gcc/linux-x86/arm/arm-linux-gnueabihf-7.5 --depth=1
 echo "Done!"
 
