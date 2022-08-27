59b81d0edb8f8daff4d3305a63e084873198e85c
techyminati <sinha.aryan03@gmail.com>
2022-08-27 17:01:26 +0530

    RMX2185: Cleanup vendorsetup config
    Change-Id: Iaaf8330270a349ffab0b203e425ec068ff17f869
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,7 @@
 echo "Cloning Dependencies"
 git config --global credential.helper cache
 git clone https://github.com/techyminati/proprietary_vendor_realmeme_RMX2185 -b android-13.0 vendor/realme/RMX2185
-git clone https://gitlab.com/techyminati/karashi -b android-11.0 kernel/realme/mt6765G
-# git clone https://github.com/techyminati/android_packages_apps_RealmeParts -b lineage-19.1 packages/apps/RealmeParts
-# git clone https://github.com/techyminati/android_packages_apps_RealmeDirac  -b lineage-19.1 packages/apps/RealmeDirac
-git clone https://github.com/techyminati/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-gnu-7.5 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-7.5 --depth=1
-git clone https://github.com/techyminati/android_prebuilts_gcc_linux-x86_arm_arm-linux-gnueabihf-7.5 prebuilts/gcc/linux-x86/arm/arm-linux-gnueabihf-7.5 --depth=1
+git clone https://github.com/techyminati/android_packages_apps_RealmeParts -b android-13.0 packages/apps/RealmeParts
 # rm -rf packages/apps/FMRadio
 # git clone https://github.com/techyminati/android_packages_apps_FMRadio packages/apps/FMRadio
 echo "Done!"
