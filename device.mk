0075af4f41f679bcd5cb22c89570436c26d5aaa6
Mathieu Chartier <mathieuc@google.com>
2022-11-14 19:52:52 +0530

    RMX2185: Speed profile services and wifi-service to reduce RAM  and storage
    Change-Id: Icca4a7a198db0beab37570f514ed358354e62771
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -48,6 +48,9 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # ThemePicker
 PRODUCT_PACKAGES += \
     ThemePicker \
