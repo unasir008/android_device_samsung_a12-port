c6cecc0b5966486101ac67e8c5003fa6387d5d6e
Aryan Sinha <sinha.aryan03@gmail.com>
2023-10-09 18:12:59 +0530

    RMX2185: vendorsetup: Uprev to U

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 echo "Cloning Dependencies"
-git config --global credential.helper cache
-git clone https://github.com/techyminati/proprietary_vendor_realmeme_RMX2185 -b android-13.0 vendor/realme/RMX2185
-git clone https://github.com/techyminati/android_packages_apps_RealmeParts -b android-13.0 packages/apps/RealmeParts
+#git config --global credential.helper cache
+git clone https://github.com/techyminati/proprietary_vendor_realmeme_RMX2185 -b android-14.0 vendor/realme/RMX2185
+#git clone https://github.com/techyminati/android_packages_apps_RealmeParts -b android-13.0 packages/apps/RealmeParts
 # rm -rf packages/apps/FMRadio
 # git clone https://github.com/techyminati/android_packages_apps_FMRadio packages/apps/FMRadio
 echo "Done!"
