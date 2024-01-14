0b6aa30cbd55172d748009e7c7910c1a3bcaf3d4
Aryan Sinha <sinha.aryan03@gmail.com>
2024-01-14 11:22:16 +0530

    RMX2185: Kang some Bluetooth properties from `device/phh/treble`

--- system_prop.mk
+++ system_prop.mk
@@ -162,6 +162,28 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
      dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5 \
      dalvik.vm.dex2oat-threads=6
 
+# Bluetooth
+PRODUCT_PRODUCT_PROPERTIES += \
+    bluetooth.device.class_of_device?=90,2,12 \
+    bluetooth.hardware.power.operating_voltage_mv=3300 \
+    bluetooth.profile.asha.central.enabled?=true \
+    bluetooth.profile.avrcp.target.enabled?=true \
+    bluetooth.profile.a2dp.source.enabled?=true \
+    bluetooth.profile.bas.client.enabled?=true \
+    bluetooth.profile.gatt.enabled?=true \
+    bluetooth.profile.hfp.ag.enabled?=true \
+    bluetooth.profile.hid.device.enabled?=true \
+    bluetooth.profile.hid.host.enabled?=true \
+    bluetooth.profile.map.server.enabled?=true \
+    bluetooth.profile.opp.enabled?=true \
+    bluetooth.profile.pan.nap.enabled?=true \
+    bluetooth.profile.pan.panu.enabled?=true \
+    bluetooth.profile.pbap.server.enabled?=true \
+    bluetooth.profile.sap.server.enabled?=true
+
 PRODUCT_SYSTEM_PROPERTIES += \
-    persist.bluetooth.system_audio_hal.enabled=11
+    persist.bluetooth.system_audio_hal.enabled=true \
+    persist.bluetooth.bluetooth_audio_hal.disabled=false \
+    persist.bluetooth.a2dp_offload.disabled=true \
+    ro.bluetooth.a2dp_offload.supported=false
 
